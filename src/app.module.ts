1bee0094cb7cdf46282ff447ba539533c0338d6e
stLmpp <gui.stlmpp@gmail.com>
2024-04-29 20:37:07 -0300

    fix: FirebaseAdminModule is required to use eventarc service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { RedisModule } from '@st-achievements/core';
 import { DrizzleOrmModule } from '@st-achievements/database';
 import { CoreModule } from '@st-api/core';
+import { FirebaseAdminModule } from '@st-api/firebase';
 
 import { AppHandler } from './app.handler.js';
 import { LockService } from './lock.service.js';
 import { PlatinumService } from './platinum.service.js';
 
 @Module({
-  imports: [CoreModule.forRoot(), DrizzleOrmModule, RedisModule],
+  imports: [CoreModule.forRoot(), DrizzleOrmModule, RedisModule, FirebaseAdminModule.forRoot()],
   controllers: [],
   providers: [AppHandler, PlatinumService, LockService],
 })
