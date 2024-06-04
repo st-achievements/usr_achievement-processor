ed92dadcd10c9cc6ff05d0b96105d3f20ceb0b29
stLmpp <gui.stlmpp@gmail.com>
2024-06-04 08:55:35 -0300

    fix: add dayjs.extend(weekOfYear) to vitest setup

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,4 +1,6 @@
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat.js';
+import weekOfYear from 'dayjs/plugin/weekOfYear.js';
 
 dayjs.extend(customParseFormat);
+dayjs.extend(weekOfYear);
